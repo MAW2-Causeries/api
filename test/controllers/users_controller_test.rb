7e2d578935e56b18205c255dcfecc80d82e72393
NathanChauveau <nathan.chauveau@eduvaud.ch>
2025-12-08 11:38:18 +0100

    feat: add 3 failure tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -15,10 +15,26 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_equal users(:one).username, response["username"]
   end
 
+    test "login_failure_wrong_input" do
+    post "/api/sessions", params: { email: "teest@test.com", password: "test" }
+
+    assert_response(:unauthorized)
+    response = JSON.parse(@response.body)
+    assert_equal "Invalid password or email", response["error"]
+  end
+
   test "register_success" do
     post "/api/users", params: { user: { email: "testing@test.com", password: "password", username: "testoting" } }
 
     assert_response(:success)
+  end
+
+  test "register_failure_duplicate" do
+    post "/api/users", params: { email: "test@test.com", password: "test" }
+
+    assert_response(:forbidden)
+    response = JSON.parse(@response.body)
+    assert_equal "The username and/or email has already be taken", response["error"]
   end
 
   test "check_token_success" do
@@ -30,5 +46,12 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     response = JSON.parse(@response.body)
 
     assert_equal users(:two).username, response["username"]
+  end
+
+  test "check_token_failure_not_found" do
+    get "/api/sessions", params: { Authorization: "I am the token" }
+    assert_response(:unauthorized)
+    response = JSON.parse(@response.body)
+    assert_equal "Authentification error: Invalid token", response["error"]
   end
 end
