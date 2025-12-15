ef37f463dfe238e4aebd75ddbde2247ea097aea5
NathanChauveau <nathan.chauveau@eduvaud.ch>
2025-12-15 08:55:12 +0100

    chore: fix CI for 2 + fix test model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
   test "downcases and strips email_address" do
-    user = User.new(email_address: " DOWNCASED@EXAMPLE.COM ")
-    assert_equal("downcased@example.com", user.email_address)
+    user = User.new(email: " DOWNCASED@EXAMPLE.COM ")
+    assert_equal("downcased@example.com", user.email)
   end
 end
