970778ed27bfa6767338ff8c01da205069e19f60
NathanChauveau <nathan.chauveau@eduvaud.ch>
2026-01-05 10:51:14 +0100

    test: just a test to see reutrn result

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -46,7 +46,7 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
     get "/api/sessions", params: { Authorization: token }
     response = JSON.parse(@response.body)
 
-    assert_equal users(:two).username, response["user"]["username"]
+    assert_equal users(:two).username, response
   end
 
   test "check_token_failure_not_found" do
