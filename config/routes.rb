86bb221b9f0572d2c1d486d0fcdcbda63ba51995
NathanChauveau <nathan.chauveau@eduvaud.ch>
2026-01-05 10:43:15 +0100

    fix: remove files as they weren't use by anything (+ update route)

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,6 @@ Rails.application.routes.draw do
     resources :sessions, only: %i[create index destroy]
     resources :users, only: %i[show update create destroy]
   end
-
-  resources :passwords, param: :token
-
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
