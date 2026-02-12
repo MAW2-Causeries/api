b61a8ed199ccc46951bfdb74aeaf4437cb115c6c
NathanChauveau <nathan.chauveau@eduvaud.ch>
2026-02-12 16:32:05 +0100

    feat: add remaining methods for channel

--- app/controllers/api/channels_controller.rb
+++ app/controllers/api/channels_controller.rb
@@ -2,7 +2,6 @@ module Api
   class ChannelsController < ApplicationController
       protect_from_forgery with: :null_session
 
-
     def show
       begin
         channel = Channel.find_by!(uuid: params[:id])
@@ -23,9 +22,23 @@ module Api
     end 
     
     def update
+      channel = Channel.find_by(uuid: params[:id])
+
+      begin
+        channel.update_columns({ name: params[:name], category: params[:category], description: params[:description] })
+        head :ok
+      rescue NoMethodError
+        render json: InvalideUserData.new, status: :unprocessable_entity
+      end
     end 
 
     def destroy
+      begin
+        Channel.find_by!(uuid: params[:id]).destroy
+        head :ok
+      rescue ActiveRecord::RecordNotFound
+        render json: ChannelNotFound.new, status: :not_found #todo
+      end
     end
 
     def channel_params
