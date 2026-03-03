e734f6abc1210e030571b862f485a39a352462cb
NathanChauveau <nathan.chauveau@eduvaud.ch>
2026-03-03 15:12:26 +0100

    feat: controller for guild

--- app/controllers/api/guilds_controller.rb
+++ app/controllers/api/guilds_controller.rb
@@ -1,15 +1,59 @@
 module Api
   class GuildsController < ApplicationController
+    protect_from_forgery with: :null_session
+
     def show
-    end 
+      begin
+        guild = Guild.find_by!(uuid: params[:id])
+        render json: guild.as_json, status: :ok
+      rescue ActiveRecord::RecordNotFound
+        render json: GuildNotFound.new, status: :not_found
+      end
+    end
 
     def create
-    end 
-    
+      attrs = guild_params.to_h
+      guild = Guild.new(attrs)
+      begin
+        guild.save!
+        head :ok
+      rescue ActiveRecord::InvalidForeignKey
+        render json: UserNotFound.new, status: :unprocessable_entity
+      rescue ActiveRecord::RecordInvalid
+        if User.find_by(uuid: guild.owner_id).nil? || User.find_by(uuid: guild.creator_id).nil?
+          render json: UserNotFound.new, status: :unprocessable_entity
+        else
+          render json: DuplicateGuild.new, status: :unprocessable_entity
+        end
+      end
+    end
+
     def update
-    end 
+      begin
+        guild = Guild.find_by!(uuid: params[:id])
+        guild.update_columns({ name: params[:name], description: params[:description], owner_id: params.fetch(:owner_id, guild.owner_id) , banner_picture_path: params[:banner_picture_path] })
+        head :ok
+      rescue NoMethodError
+        render json: InvalidGuildData.new, status: :unprocessable_entity
+      rescue ActiveRecord::RecordInvalid, ActiveRecord::RecordNotUnique
+        render json: DuplicateGuild.new, status: :unprocessable_entity
+      rescue ActiveRecord::RecordNotFound
+        render json: GuildNotFound.new, status: :not_found
+      end
+    end
 
     def destroy
+      begin
+        Guild.find_by!(uuid: params[:id]).destroy
+        head :ok
+      rescue ActiveRecord::RecordNotFound
+        render json: GuildNotFound.new, status: :not_found
+      end
+    end
+
+    def guild_params
+      params.require(:guild).permit(:name, :description, :owner_id, :banner_picture_path, :creator_id)
+      # might be able to refactor/reduce to asked id
     end
   end
 end
