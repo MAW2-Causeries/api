4d6c10f482875b5d0edf44281d01c43aca1e5a48
Nathan <nathan.chauveau@eduvaud.ch>
2026-03-05 15:57:48 +0100

    refactor: refactor guild

--- app/controllers/api/guilds_controller.rb
+++ app/controllers/api/guilds_controller.rb
@@ -2,13 +2,12 @@ module Api
   class GuildsController < ApplicationController
     protect_from_forgery with: :null_session
 
+    rescue_from ActiveRecord::RecordNotFound, with: :guild_not_found
+    rescue_from ActiveRecord::NoMethodError, with: :record_invalid
+    rescue_from ActiveRecord::InvalidForeignKey, with: :user_not_found
+
     def show
-      begin
-        guild = Guild.find_by!(uuid: params[:id])
-        render json: guild.as_json, status: :ok
-      rescue ActiveRecord::RecordNotFound
-        render json: GuildNotFound.new, status: :not_found
-      end
+        render json: set_guild.as_json, status: :ok
     end
 
     def create
@@ -17,8 +16,6 @@ module Api
       begin
         guild.save!
         head :ok
-      rescue ActiveRecord::InvalidForeignKey
-        render json: UserNotFound.new, status: :unprocessable_entity
       rescue ActiveRecord::RecordInvalid
         if User.find_by(uuid: guild.owner_id).nil? || User.find_by(uuid: guild.creator_id).nil?
           render json: UserNotFound.new, status: :unprocessable_entity
@@ -30,15 +27,10 @@ module Api
 
     def update
       begin
-        guild = Guild.find_by!(uuid: params[:id])
-        guild.update_columns({ name: params[:name], description: params[:description], owner_id: params.fetch(:owner_id, guild.owner_id) , banner_picture_path: params[:banner_picture_path] })
+        set_guild.update!({ name: params[:name], description: params[:description], owner_id: params.fetch(:owner_id, guild.owner_id), banner_picture_path: params[:banner_picture_path] })
         head :ok
-      rescue NoMethodError
-        render json: InvalidGuildData.new, status: :unprocessable_entity
       rescue ActiveRecord::RecordInvalid, ActiveRecord::RecordNotUnique
         render json: DuplicateGuild.new, status: :unprocessable_entity
-      rescue ActiveRecord::RecordNotFound
-        render json: GuildNotFound.new, status: :not_found
       end
     end
 
@@ -46,14 +38,28 @@ module Api
       begin
         Guild.find_by!(uuid: params[:id]).destroy
         head :ok
-      rescue ActiveRecord::RecordNotFound
-        render json: GuildNotFound.new, status: :not_found
       end
     end
 
     def guild_params
       params.require(:guild).permit(:name, :description, :owner_id, :banner_picture_path, :creator_id)
       # might be able to refactor/reduce to asked id
+    end
+
+    def set_guild
+      Guild.find_by!(uuid: params[:id])
+    end
+
+    def guild_not_found
+      render json: GuildNotFound.new, status: :not_found
+    end
+
+    def user_not_found
+      render json: UserNotFound.new, status: :unprocessable_entity
+    end
+
+    def record_invalid
+      render json: InvalidGuildData.new, status: :unprocessable_entity
     end
   end
 end
