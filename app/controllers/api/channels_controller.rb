a223c82b8d1aae8d3b2fc862a2be05b882faeee8
NathanChauveau <nathan.chauveau@eduvaud.ch>
2026-03-17 14:32:02 +0100

    refactor: entire channel controller

--- app/controllers/api/channels_controller.rb
+++ app/controllers/api/channels_controller.rb
@@ -2,13 +2,14 @@ module Api
   class ChannelsController < ApplicationController
     protect_from_forgery with: :null_session
 
+    rescue_from ActiveRecord::RecordNotFound, with: :channel_not_found
+    rescue_from ActiveRecord::InvalidForeignKey, with: :guild_not_found
+    rescue_from ActiveRecord::RecordInvalid, ActiveRecord::RecordNotUnique, with: :duplicate_record
+    rescue_from NoMethodError, with: :record_invalid
+
     def show
-      begin
         channel = Channel.find_by!(uuid: params[:id])
         render json: channel.as_json, status: :ok
-      rescue ActiveRecord::RecordNotFound
-        render json: ChannelNotFound.new, status: :not_found
-      end
     end
 
     def create
@@ -16,39 +17,39 @@ module Api
       channel = Channel.new(attrs)
       channel.name = channel.name.match?(/^([a-z0-9]+-?)*$/) ? channel.name : channel.reformatted_name
       logger.debug "Creating channel with attributes: #{channel.inspect}"
-      begin
-        channel.save!
-        head :ok
-      rescue ActiveRecord::InvalidForeignKey
-        render json: GuildNotFound.new, status: :unprocessable_entity
-      rescue ActiveRecord::RecordInvalid
-        render json: DuplicateChannel.new, status: :unprocessable_entity
-      end
+      channel.save!
+      head :ok
     end
 
     def update
       channel = Channel.find_by(uuid: params[:id])
-      begin
-        channel.update_columns({ name: params[:name], category: params[:category], description: params[:description] })
-        head :ok
-      rescue NoMethodError
-        render json: InvalidChannelData.new, status: :unprocessable_entity
-      rescue ActiveRecord::RecordInvalid, ActiveRecord::RecordNotUnique
-        render json: DuplicateChannel.new, status: :unprocessable_entity
-      end
+      channel.update_columns({ name: params[:name], category: params[:category], description: params[:description] })
+      head :ok
     end
 
     def destroy
-      begin
         Channel.find_by!(uuid: params[:id]).destroy
         head :ok
-      rescue ActiveRecord::RecordNotFound
-        render json: ChannelNotFound.new, status: :not_found
-      end
     end
 
     def channel_params
       params.require(:channel).permit(:name, :category, :description, :guild_id)
     end
+
+    def duplicate_record
+      render json: DuplicateChannel.new, status: :unprocessable_entity
+    end
+
+    def guild_not_found
+      render json: GuildNotFound.new, status: :not_found
+    end
+
+    def record_invalid
+      render json: InvalidChannelData.new, status: :unprocessable_entity
+    end 
+
+    def channel_not_found
+      render json: ChannelNotFound.new, status: :not_found
+    end 
   end
 end
